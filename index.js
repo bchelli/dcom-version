35616bd03eb543b6a4a2954c399dff5cc5db926b
Benjamin Chelli <benjamin@chelli.net>
2016-01-27 12:35:59 -0500

    Remove the skip functionnality, this is too complex to manage

--- index.js
+++ index.js
@@ -251,19 +251,14 @@ function mergeBranches (options) {
 				console.log(content);
 				return promptUserFor({
 					valid: {
-						pattern: /^(yes|no|skip)$/,
-						description: 'Please resolve the merge conflict. When you are done please type\n\t- "yes" to proceed\n\t- "no" to abort the creation of the release\n\t- "skip" to skip this branch from the release\nyes/no/skip?',
+						pattern: /^(yes|no)$/,
+						description: 'Please resolve the merge conflict. When you are done please type\n\t- "yes" to proceed\n\t- "no" to abort the creation of the release\nyes/no?',
 						required: true
 					},
 				})
 				.then(function (response) {
 					if (response.valid === 'no') {
 						throw new Error('Stop process => rollback');
-					}
-					if (response.valid === 'skip') {
-						return gitExec({
-							args: ['merge', '--abort'],
-						});
 					}
 				});
 			})
